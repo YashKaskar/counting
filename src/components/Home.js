37cff53af384539d47ca8fad80910a01b39c7a08
Yash Kaskar <yash.kaskar99@gmail.com>
2023-01-25 10:03:13 +0530

    home content

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,18 @@
 const Home = () => {
     return (    
-        <h1> Counting </h1>
+        <div>   
+            <h1> Counting......</h1>
+
+            <div className="btn">
+            <button className="btn1"> + </button>
+            <button className="btn2"> - </button>
+
+            </div>
+            <div className="number">   
+                <span> 0 </span>
+            </div>
+        </div>
+        
      );
 }
  
