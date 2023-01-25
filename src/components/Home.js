5b86acde4671f934600482665fd7f88386ad5dd3
Yash Kaskar <yash.kaskar99@gmail.com>
2023-01-25 11:13:55 +0530

    useState

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,34 @@
+import { useState } from "react";
+
 const Home = () => {
+
+    const [count, setCount] = useState(() => {  
+        console.log('run one time');
+        return(0)
+    })
+
+    function decreaseCount() {
+        setCount( lastCount => count - 1) // decreasing the count
+    }
+
+    function increaseCount() {  
+        setCount( lastCount => count + 1) // increase the count
+    }
+
+
+
+
     return (    
         <div>   
             <h1> Counting......</h1>
 
             <div className="btn">
-            <button className="btn1"> + </button>
-            <button className="btn2"> - </button>
+            <button className="btn1" onClick={increaseCount}> + </button>
+            <button className="btn2" onClick={decreaseCount} > - </button>
 
             </div>
             <div className="number">   
-                <span> 0 </span>
+                <span> {count} </span>
             </div>
         </div>
         
